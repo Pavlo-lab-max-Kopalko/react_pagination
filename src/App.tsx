8bc5d67fadef429d2ca2664bfdcf649501788822
Pavlo <oksanakopalko@gmail.com>
2025-07-10 20:38:59 +0300

    навіщо меніonPageChange без нього і так все працює?

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { getNumbers } from './utils';
 
+import { Pagination } from './components/Pagination';
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const items = getNumbers(1, 42).map(n => `Item ${n}`);
+export const items = getNumbers(1, 42).map(n => `Item ${n}`);
 
 export const App: React.FC = () => {
+  const [perItems, setPerItems] = useState<string>('3');
+  const [numberPage, setNumberPage] = useState<string>('#1');
+
+  const getNumber = (number: string) => {
+    setPerItems(number);
+  };
+
+  const resetNumberPage = () => {
+    setNumberPage('#1');
+  };
+
+  const getNumberPage = (addres: string) => {
+    const arrAddres = addres.split('/');
+
+    setNumberPage(arrAddres[arrAddres.length - 1]);
+  };
+
+  const pageQuantity = Math.ceil(items.length / +perItems);
+
+  const passedItems =
+    Number(perItems) * Number(numberPage.slice(1)) - Number(perItems);
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        Page {numberPage} (items {passedItems + 1} -{' '}
+        {Number(perItems) + passedItems} of 42)
       </p>
 
       <div className="form-group row">
@@ -19,7 +44,10 @@ export const App: React.FC = () => {
           <select
             data-cy="perPageSelector"
             id="perPageSelector"
-            className="form-control">
+            className="form-control"
+            onClick={event => getNumber(event.currentTarget.value)}
+            onChange={() => resetNumberPage()}
+          >
             <option value="3">3</option>
             <option value="5">5</option>
             <option value="10">10</option>
@@ -33,77 +61,59 @@ export const App: React.FC = () => {
       </div>
 
       {/* Move this markup to Pagination */}
+
       <ul className="pagination">
         <li className="page-item disabled">
           <a
             data-cy="prevLink"
             className="page-link"
             href="#prev"
-            aria-disabled="true">
+            aria-disabled="true"
+          >
             «
           </a>
         </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">
-            1
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">
-            2
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">
-            3
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">
-            4
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">
-            5
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">
-            6
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">
-            7
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">
-            8
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">
-            9
-          </a>
-        </li>
+
+        {Array.from({ length: pageQuantity }).map((_, i) => {
+          return (
+            <li
+              key={i}
+              className={`page-item ${numberPage === `#${i + 1}` ? 'active' : ''}`}
+            >
+              <a
+                data-cy="pageLink"
+                className="page-link"
+                href={`#${i + 1}`}
+                onClick={event => getNumberPage(event.currentTarget.href)}
+              >
+                {i + 1}
+              </a>
+            </li>
+          );
+        })}
+
         <li className="page-item">
           <a
             data-cy="nextLink"
             className="page-link"
             href="#next"
-            aria-disabled="false">
+            aria-disabled="false"
+          >
             »
           </a>
         </li>
       </ul>
       <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
+        <Pagination
+          total={42} // total number of items to paginate
+          perPage={Number(perItems)} // number of items per page
+          currentPage={Number(
+            numberPage.slice(1),
+          )} /* optional with 1 by default */
+          onPageChange={(page: { addEventListener: () => void }) => {
+            page.addEventListener();
+          }}
+        />
       </ul>
     </div>
   );
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,38 @@
-export const Pagination = () => {};
+import { items } from '../../App';
+import '../../App.css';
+
+interface Props {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+export const Pagination = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}: Props) => {
+  console.log(perPage);
+  console.log(currentPage);
+  const passedItems = perPage * currentPage - perPage; // items those we have passed in previous pages
+  // from passedItems to passedItems + perPage we must show items
+  console.log(passedItems);
+
+  const firstFiveElements = items.slice(passedItems, passedItems + perPage);
+
+  return (
+    <>
+      {firstFiveElements.map(item => {
+        console.log(item);
+
+        return (
+          <li data-cy="item" key={item}>
+            {item}
+          </li>
+        );
+      })}
+    </>
+  );
+};
